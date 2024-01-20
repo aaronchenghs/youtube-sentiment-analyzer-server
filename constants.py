9539e038e5dbb81a677e0e38ebc4dca6fc736297
Aaron Cheng <aaronchenghs@gmail.com>
2024-01-20 00:24:36 -0600

    More data, paths

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 credentials_path = './YTCreds.json'
 redirect_uri = 'http://localhost:8080/'
-service_account_file = 'C:\\Users\\vojoh\\OneDrive\\Desktop\\Projects\\youtube-sentiment-analyzer-server\\csc3730-project-team-had-0548b315ad9b.json'
-trained_model_path = 'C:\\Users\\vojoh\\OneDrive\\Desktop\\Projects\\youtube-sentiment-analyzer-server\\trained_multi_label_model.joblib'
+service_account_file = '.\\csc3730-project-team-had-0548b315ad9b.json'
+trained_model_path = '.\\trained_multi_label_model.joblib'
